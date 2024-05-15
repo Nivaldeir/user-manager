bf97ae0e9718418aac964d50e31b0f03507df4c7
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-05-15 13:58:45 -0300

    feat: adding caching

---
+++ back/src/infra/cache/ICache.ts
@@ -0,0 +1,5 @@
+export interface ICache {
+  Get(key: string): Promise<any>
+  Set(key: string, value: any, ttl?: number): Promise<void>
+  delete(key: string): Promise<void>
+}
---
+++ back/src/infra/cache/RedisAdapter.ts
@@ -0,0 +1,33 @@
+import { Redis } from "ioredis";
+import { ICache } from "./ICache";
+import Logger from "../logger";
+
+export class RedisAdapter extends Redis implements ICache {
+  constructor() {
+    super({
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT!),
+      password: process.env.REDIS_PASSWORD,
+    })
+    super.on("error", (err: Error) => {
+      Logger.instance.error(`REDIS : ${err.message}`);
+    });
+    super.on("connect", () => {
+      Logger.instance.success("REDIS : Connected");
+    });
+  }
+  async delete(key: string) {
+    await this.del(key)
+  }
+  async Get(value: string): Promise<string | null> {
+    return this.get(value);
+  }
+  async Set(key: string, value: string, ttl?: number): Promise<void> {
+    if (ttl) {
+      this.set(key, value, "EX", ttl);
+    } else {
+      this.set(key, value);
+    }
+  }
+}
+
