30cab39d0f2334fd0ae041b057f76a0f207d29a9
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-06-25 16:26:57 -0300

    Save local changes before pulling

--- back/test/integration/login-auth-history.test.ts
+++ back/test/integration/login-auth-history.test.ts
@@ -1,5 +1,6 @@
 import { LoginHistoryAuthentication } from "../../src/core/domain/entities/login-authentication"
 import { CreateLoginAuth } from "../../src/core/domain/use_cases/login/create-login-auth"
+import { FindUserHistoryAuth } from "../../src/core/domain/use_cases/login/find-user-history-auth"
 import { PgAdapter } from "../../src/infra/database/PgAdapter"
 import LoginAuthHistoryDatabase from "../../src/infra/repository/login-auth-history"
 
@@ -19,10 +20,17 @@ describe("Permission with testcontainer", ()=>{
   afterAll(async () =>{
     client.close()
   })
-  test("should create permission", async ()=>{
+  test("should create login auth history", async ()=>{
     const CreateUsecase = new CreateLoginAuth(dbAdapter)
     const output = await  CreateUsecase.execute({...input, userId: "ca7ac207-999a-4ac6-9b64-13406645a3c7"})
     expect(output.device).toBe(input.device)
     expect(output.id).toBeDefined()
+  })
+  test("should find login auth history for user", async ()=>{
+    const CreateUsecase = new CreateLoginAuth(dbAdapter)
+    const outputAuth = await  CreateUsecase.execute({...input, userId: "ca7ac207-999a-4ac6-9b64-13406645a3c7"})
+    const findUserCase = new FindUserHistoryAuth(dbAdapter);
+    const output = await findUserCase.execute({ userId: outputAuth.id })
+    expect(output).toBeInstanceOf(Array<LoginHistoryAuthentication>)
   })
 })
